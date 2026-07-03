8e03a3f38777a0c6535e0f82245ab1b3ae629b57
36Venky <venkateshraju761@gmail.com>
2026-07-03 09:15:51 +0530

    Updated Controller

--- Controller/Analyzer.py
+++ Controller/Analyzer.py
@@ -4,32 +4,25 @@ import logging
 from datetime import datetime, timedelta, time as dtime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
-# Environmental Variables 
+# Environmental Variables
 from dotenv import load_dotenv
-load_dotenv() 
+load_dotenv()
 
 # Dependencies
 from Data_Manager import *
+from Dependencies.Utils import *
+from Strategy import *
 
 def Analyzer(tickers):
-    
-    now = datetime.now().time()
-    start_time = dtime(9,27)
-    end_time   = dtime(15, 35)
 
-    T = 1
-
-    if T == 0:
-        if not (start_time <= now <= end_time and 0 <= datetime.now().weekday() <= 4):
-            return  # Market closed
-    
+    start = time.perf_counter()
     Download(tickers)
-
-    INTERVAL = "15m"
+    end = time.perf_counter()
+    write("T(N).txt",f"{datetime.now().strftime("%H:%M:%S")},{end - start:.2f}\n")
 
     for ticker in tickers:
-        df = get_data(ticker,INTERVAL)
-        if df is None:
-            continue
-        print(df.tail(1))
+        Regression(ticker)
+
+    scan_breakouts(tickers)
+
+    wait_until_next_candle("15m")
